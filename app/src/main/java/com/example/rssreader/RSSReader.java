f178e1602d8f6f7acbed2858f50874f4c5e76a1f
Ivanov Nikita <scrinoman@gmail.com>
2015-11-28 15:29:48 +0300

    Initial

---
+++ app/src/main/java/com/example/rssreader/RSSReader.java
@@ -0,0 +1,11 @@
+package com.example.rssreader; /**
+ * Created by Никита on 28.11.2015.
+ */
+import java.io.*;
+import java.net.*;
+
+public class RSSReader {
+    RSSReader(){};
+
+
+}
---
+++ app/src/main/java/com/example/rssreader/RssItem.java
@@ -0,0 +1,52 @@
+package com.example.rssreader;
+
+/**
+ * Created by Никита on 28.11.2015.
+ */
+public class RssItem {
+    private String title;
+    private String description;
+    private String link;
+    private String author;
+    private String guid;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+}
